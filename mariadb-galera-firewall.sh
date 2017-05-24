4e177af96070096b9f2ea0783d1550b0b65d1031
manhof <manhof@users.noreply.github.com>
2017-05-24 08:57:52 -0400

    Add files via upload

---
+++ mariadb-galera-firewall.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+serverbuild= gawk -F= '/^ID=/{print $2}' /etc/os-release
+service="Mariadb-Galera"
+title= "Mariadb Clustering Ports"
+description="Mariadb Ports required for running Mariadb-Galera which include 3306,4567,4568,4444/tcp & 4567/udp"
+port1="3306"
+port2="4567"
+port3="4568"
+port4="4444"
+protocol="tcp"
+protocol2="udp"
+echo "Firewall Configuration" >> /home/test
+if [[ $serverbuild == *"ubuntu"* ]]
+ then
+	echo "[$service]" >> /etc/ufw/applications.d/$service
+    echo "title=$title" >> /etc/ufw/applications.d/$service
+    echo "description=$description" >> /etc/ufw/applications.d/$service
+    echo "ports=$port1,$port2,$port3,$port4/$protocol|$port2/$protocol2" >> /etc/ufw/applications.d/$service
+    ufw update $service
+	ufw allow in OpenSSH
+    ufw allow in $service
+    ufw enable
+    ufw status >> /home/test
+elif [[ $serverbuild == *"centos"* ]]
+ then
+	dnf install firewalld -y
+	echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>" >> /etc/firewalld/services/$service.xml
+	echo "<service>" >> /etc/firewalld/services/$service.xml
+	echo "   <short>$title</short>" >> /etc/firewalld/services/$title.xml
+	echo "   <description>$description</description>" >> /etc/firewalld/services/$service.xml
+	echo "   <port protocol=\"$protocol\" port=\"$port1\"/>" >> /etc/firewalld/services/$service.xml
+	echo "   <port protocol=\"$protocol\" port=\"$port2\"/>" >> /etc/firewalld/services/$service.xml	
+	echo "   <port protocol=\"$protocol\" port=\"$port3\"/>" >> /etc/firewalld/services/$service.xml
+	echo "   <port protocol=\"$protocol\" port=\"$port4\"/>" >> /etc/firewalld/services/$service.xml
+	echo "   <port protocol=\"$protocol2\" port=\"$port2\"/>" >> /etc/firewalld/services/$service.xml	
+	echo "</service>" >> /etc/firewalld/services/$service.xml
+	firewall-offline-cmd --zone=public --add-interface=eth0
+	firewall-offline-cmd --set-default-zone=public
+	firewall-offline-cmd --zone=public --add-service=ssh
+	firewall-offline-cmd --zone=public --add-service=$service
+	echo "Default Zone" >> /home/test
+	firewall-offline-cmd --get-default-zone >> /home/test
+	firewall-offline-cmd --info-zone=public >> /home/test
+	systemctl start firewalld
+	systemctl enable firewalld
+else
+	echo "Cannot determine Build Type... Exiting" >> /home/test
+	exit 3
+fi  
+echo "END FIREWALL CONFIG" >> /home/test
+
---
+++ mariadb-galera-setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+serverbuild= gawk -F= '/^ID=/{print $2}' /etc/os-release
+masterb=$1
+cluster_name=$2
+pub=$3
+hostname=$4
+master=$5
+if [[ $serverbuild == *"ubuntu"* ]]
+ then
+	location=/etc/mysql/conf.d/galera.cnf
+	wsrep_provider=/usr/lib/galera/libgalera_smm.so
+elif [[ $serverbuild == *"centos"* ]]
+ then
+	location=/etc/my.cnf.d/server.cnf
+	cp $location /home/server.bak
+	rm -rf $location
+	wsrep_provider=/usr/lib64/galera/libgalera_smm.so
+else
+
+	echo "Cannot determine Build Type... Exiting" >> /home/test
+	exit 3
+fi
+echo "configuring database info" >> /home/test
+echo "[mysqld]" >> $location
+echo "binlog_format=ROW" >> $location
+echo "default-storage-engine=innodb" >> $location
+echo "innodb_autoinc_lock_mode=2" >> $location
+echo "bind-address=0.0.0.0" >> $location
+echo "" >> $location
+if [ $masterb -eq 1 ]
+ then
+	echo "Setting Up Server As Master Server" >> /home/test
+	echo "Galera Provider Configuration" >> $location
+	echo "wsrep_on=ON" >> $location
+    echo "wsrep_provider=$wsrep_provider" >> $location
+	echo "" >> $location
+    echo "# Galera Cluster Configuration" >> $location
+    echo "wsrep_cluster_name=\"$cluster_name\"" >> $location
+    echo "wsrep_cluster_address=\"gcomm://$pub\"" >> $location
+    echo "" >> $location
+    echo "# Galera Synchronization Configuration" >> $location
+    echo "wsrep_sst_method=rsync" >> $location
+    echo "" >> $location
+    echo "# Galera Node Configuration" >> $location
+    echo "wsrep_node_address=\"$pub\"" >> $location
+    echo "wsrep_node_name=\"$hostname\"" >> $location
+    echo "" >> $location
+    galera_new_cluster
+else
+	echo "Setting Up Server as slave" >> /home/test
+	echo "Galera Provider Configuration" >> $location
+    echo "wsrep_on=ON" >> $location
+    echo "wsrep_provider=$wsrep_provider" >> $location
+	echo "" >> $location
+    echo "# Galera Cluster Configuration" >> $location
+    echo "wsrep_cluster_name=\"$cluster_name\"" >> $location
+    echo "wsrep_cluster_address=\"gcomm://$master,$pub\"" >> $location
+    echo "" >> $location
+    echo "# Galera Synchronization Configuration" >> $location
+    echo "wsrep_sst_method=rsync" >> $location
+    echo "" >> $location
+    echo "# Galera Node Configuration" >> $location
+    echo "wsrep_node_address=\"$pub\"" >> $location
+    echo "wsrep_node_name=\"$hostname\"" >> $location
+    echo "" >> $location
+    systemctl start mysql
+fi
