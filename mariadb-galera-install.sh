f50b756df4f524d752761868ee84b82177edcdc7
manhof <manhof@users.noreply.github.com>
2017-05-30 14:29:13 -0400

    Update mariadb-galera-install.sh

--- mariadb-galera-install.sh
+++ mariadb-galera-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-serverbuild= gawk -F= '/^ID=/{print $2}' /etc/os-release
+gawk -F= '/^ID=/{print $2}' /etc/os-release > /home/id.txt
+serverbuild=$(cat /home/id.txt)
+echo " This is the Server Build: " $serverbuild >> /home/test
 datastore=$1
 if [[ $serverbuild == *"ubuntu"* ]]
  then
